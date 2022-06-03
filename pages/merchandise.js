7ac9c8f125e4cfb7bc199aa94108df3cad7d5d42
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-06-03 15:20:44 -0700

    link merch to shopify

--- pages/merchandise.js
+++ pages/merchandise.js
@@ -1,9 +1,16 @@
 import styles from "../styles/Home.module.css";
 
+import React, { Component, useEffect } from "react";
+
 export default function Merch() {
+  useEffect(() => {
+    window.location.href = "https://the-psychotic-break.myshopify.com/";
+  }, []);
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Merch Coming Soon!</h1>
+      <h1 className={styles.title}>
+        Hang On, we're sending you to our merch store!
+      </h1>
     </main>
   );
 }
